5c905f44e65bd124c8f4e861b768169e53f1015c
Antoine Aumjaud <antoine.aumjaud@socgen.com>
2024-01-15 16:50:49 +0100

    :shower: clean non windows build code

--- update.js
+++ update.js
@@ -1,26 +1,21 @@
 const fs = require('fs');
 
-const packageJsonOpenLens = require('./lens/open-lens/package.json');
+const packageJson = JSON.parse(fs.readFileSync('./lens/open-lens/package.json'));
 
-packageJsonOpenLens.build.publish = [{
-    url: "https://github.com/societe-generale/OpenLens/releases/download/Latest",
-    provider: "generic"
+delete packageJson.scripts.postinstall;
+
+packageJson.version = `${packageJson.version}-${process.env.BUILD_ID}`;
+console.log(`Set build version: ${packageJson.version}`)
+
+packageJson.build.npmRebuild = true;
+
+packageJson.build.publish = [{
+    url: `https://github.com/societe-generale/OpenLens/releases/download/${packageJson.version}`,
+    provider: 'generic'
 }];
 
-packageJsonOpenLens.version = `${packageJsonOpenLens.version}-${process.env.BUILD_ID}`;
-packageJsonOpenLens.build.npmRebuild = true;
-packageJsonOpenLens.build.detectUpdateChannel = false;
-packageJsonOpenLens.build.beforeBuild = '../../build-hooks/beforeBuild';
+packageJson.copyright += '\n\n' 
+    + 'Binary application builds @ societe-generale/openlens-build\n',
+    + 'by Societe Generale';
 
-delete packageJsonOpenLens.scripts.postinstall;
-
-packageJsonOpenLens.copyright = [
-    packageJsonOpenLens.copyright,
-    '',
-    'Binary application builds @ societe-generale/openlens-build',
-    'by Societe Generale'
-].join("\r\n")
-
-console.log(`Set build version: ${packageJsonOpenLens.version}`)
-
-fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packageJsonOpenLens, null, 2));
+fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packageJson, null, 2));
